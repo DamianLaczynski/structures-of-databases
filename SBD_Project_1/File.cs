3f29afd5cb483f2add741debbf37a0dd3e6186c5
DamianLaczynski <damian.laczynski.edu@gmail.com>
2023-11-06 18:44:11 +0100

    Add project files.

---
+++ SBD_Project_1/File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBD_Project_1
+{
+    internal class File
+    {
+        //contains nuber of read and write operation on file and handler to file
+    }
+}
