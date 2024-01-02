0a830417e98657b301949b3374945c0ef955f735
DamianLaczynski <damian.laczynski.edu@gmail.com>
2024-01-02 12:44:55 +0100

    Add files classes with basic operations

---
+++ SBD_Project_1/BinaryFile.cs
@@ -0,0 +1,138 @@
+﻿using SBD_Project_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBD_Project_1
+{
+    internal class BinaryFile
+    {
+        private readonly int _blockSize = Configuration.FileOrganization.PageSize;
+        private string _path { get; set; }
+
+        public BinaryFile(string path)
+        {
+            _path = path;
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            using (File.Create(path)) ;
+
+        }
+        public BinaryFile(string path, FileMode mode)
+        {
+            _path = path;
+            switch(mode)
+            {
+                case FileMode.Open:
+                    if (!File.Exists(path))
+                    {
+                        using (File.Create(path)) ;
+                    }
+                    break;
+                case FileMode.Create:
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+                    using (File.Create(path)) ;
+                    break;
+                default:
+                    throw new ArgumentException("This mode is not supported");
+                    
+            }
+        }
+        public byte[] ReadPage(int pageNumber)
+        {
+            //init table with zeros
+            byte[] block = new byte[_blockSize];
+            for (int i = 0; i < _blockSize; i++)
+            {
+                block[i] = 0;
+            }
+
+            using (var stream = File.Open(_path, FileMode.Open))
+            {
+                //set stream position to begin of page
+                stream.Position = pageNumber * _blockSize;
+
+                using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                {
+                    reader.Read(block, 0, _blockSize);
+                }
+            }
+
+            return block;
+        }
+        //WARNING: to verify
+        public void WritePage(int pageNumber, byte[] block)
+        {
+            if (block.Length != _blockSize)
+            {
+                var newBlock = new byte[_blockSize];
+                for (int i = 0; i < block.Length; i++)
+                {
+                    newBlock[i] = block[i];
+                }
+                for (int i = block.Length; i < _blockSize; i++)
+                {
+                    newBlock[i] = 0;
+                }
+            }
+            using (var stream = File.Open(_path, FileMode.Open))
+            {
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8, false))
+                {
+                    //set stream position to begin of page
+                    stream.Position = pageNumber * _blockSize;
+                    writer.Write(block);
+                }
+            }
+        }
+
+        public void AllocateDiskSpace(int pageCount)
+        {
+            using (var stream = File.Open(_path, FileMode.Create))
+            {
+                //fill file with zeros
+                for (long i = 0; i < _blockSize*pageCount; i++)
+                {
+                    stream.WriteByte(0);
+                }
+            }
+        }
+
+        public int GetPageCount()
+        {
+            using (var stream = File.Open(_path, FileMode.Open))
+            {
+                return (int)Math.Ceiling((double)stream.Length / _blockSize);
+            }
+        }
+
+        public byte[] Read()
+        {
+            byte[] content;
+
+            using (var stream = File.Open(_path, FileMode.Open))
+            {
+                long blockSize = stream.Length;
+                using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                {
+                    content = new byte[blockSize];
+                    reader.Read(content, 0, (int)blockSize);
+                }
+            }
+
+            return content;
+        }
+
+        ~BinaryFile()
+        {
+            File.Delete(_path);
+        }
+    }
+}
---
+++ SBD_Project_1/OverflowAreaFile.cs
@@ -0,0 +1,166 @@
+﻿using SBD_Project_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBD_Project_1
+{
+    internal class OverflowAreaFile
+    {
+        public int MaxRecords { get; set; }
+        public int MaxIndex { get; set; }
+        public int Writes { get; set; }
+        public int Reads { get; set; }
+        public int RecordsCount { get; set; }
+        public int _pageCount { get; set; }
+
+        private static int OverflowAreaFileNo { get; set; } = 0;
+
+        private BinaryFile _file;
+
+        private LinkedList<Record> _records;
+
+        public OverflowAreaFile() 
+        {
+            _file = new BinaryFile(@$"fileOrganization\overflow{OverflowAreaFileNo}.bin");
+            _records = new LinkedList<Record>();
+            _pageCount = _file.GetPageCount();
+            OverflowAreaFileNo++;
+        }
+
+        public OverflowAreaFile(int pagesCount)
+        {
+            _file = new BinaryFile(@$"fileOrganization\overflow{OverflowAreaFileNo}.bin", FileMode.Create);
+            _records = new LinkedList<Record>();
+            //_pageCount = pagesCount; ??? check this
+            OverflowAreaFileNo++;
+        }
+        private void LoadPage(int pageNo)
+        {
+            var page = _file.ReadPage(pageNo);
+            _records = ArrayConverter.ToLinkedListRecords(page);
+            Reads++;
+        }
+        private void SavePage(int pageNo)
+        {
+            _file.WritePage(pageNo, ArrayConverter.ToByteArray(_records));
+            Writes++;
+        }
+        public void ResetOperationCount()
+        {
+            Reads = 0;
+            Writes = 0;
+        }
+
+        public void AddRecord(Record record)
+        {
+            LoadPage(_pageCount);
+
+            //add new page if there is no space
+            if(_records.Count == Configuration.FileOrganization.OverflowAreaRecordsCount)
+            {
+                _pageCount++;
+                _records = new LinkedList<Record>();
+            }
+
+            _records.AddLast(record);
+            RecordsCount++;
+            SavePage(_pageCount);
+        }
+        public void AddRecord(int pointer, Record record)
+        {
+            bool isPointerGreater = false;
+            if(pointer > record.Key)
+            {
+                isPointerGreater = true;
+                record.OverflowPointer = pointer;
+            }
+
+            this.AddRecord(record);
+
+            var pageNo = 0;
+            while(true)
+            {
+                LoadPage(pageNo);
+                var recordToUpdate = _records.FirstOrDefault(x => x.Key == pointer);
+                if(recordToUpdate is not null)
+                {
+                    if(isPointerGreater)
+                    {
+                        recordToUpdate.OverflowPointer = -1;
+                    }
+                    else
+                    {
+                        recordToUpdate.OverflowPointer = record.Key;
+                    }
+                    SavePage(pageNo);
+                    return;
+                }
+                pageNo++;
+            }
+        }
+        public Record GetRecord(int key)
+        {
+            Record record;
+            for(int i = 0; i <= _pageCount; i++)
+            {
+                LoadPage(i);
+                record = _records.FirstOrDefault(x => x.Key == key);
+                if(record is not null)
+                {
+                    return record;
+                }
+            }
+            throw new Exception("Key not found");
+        }
+        public Record GetNextRecord(int key)
+        {
+            Record record;
+            for (int i = 0; i <= _pageCount; i++)
+            {
+                LoadPage(i);
+                record = _records.FirstOrDefault(x => x.Key == key);
+                if (record is not null)
+                {
+                    _records.Remove(record);
+                    RecordsCount--;
+                    return record;
+                }
+            }
+            throw new Exception("Key not found");
+        }
+        public void RemoveRecord(int key)
+        {
+            Record record;
+            for(int i = 0; i <= _pageCount; i++)
+            {
+                LoadPage(i);
+                record = _records.FirstOrDefault(x => x.Key == key);
+                if(record is not null)
+                {
+                    _records.Remove(record);
+                    SavePage(i);
+                    RecordsCount--;
+                    return;
+                }
+            }
+            throw new Exception("Key not found");
+        }
+        public void Display()
+        {
+            var currentPageNo = 0;
+            Console.WriteLine("Overflow File");
+            while (currentPageNo <= _pageCount)
+            {
+                LoadPage(currentPageNo);
+                foreach (var record in _records)
+                {
+                    Console.WriteLine(record.ToString());
+                }
+                currentPageNo++;
+            }
+        }
+    }
+}
