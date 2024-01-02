24dd3158709ad75e7b874beef38e6066f39c5a20
DamianLaczynski <damian.laczynski.edu@gmail.com>
2024-01-02 12:44:11 +0100

    Add Result class

---
+++ SBD_Project_1/Result.cs
@@ -0,0 +1,47 @@
+﻿using SBD_Project_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBD_Project_1
+{
+    enum ResultType
+    {
+        Success,
+        Failure,
+        AlreadyExists,
+    }
+    internal class Result
+    {
+        public Result()
+        {
+            Type = ResultType.Success;
+            Reads = 0;
+            Writes = 0;
+        }
+        public Result(int reads, int writes)
+        {
+            Type = ResultType.Success;
+            Reads = reads;
+            Writes = writes;
+        }
+        public Result(int reads, int writes, ResultType result)
+        {
+            Type = result;
+            Reads = reads;
+            Writes = writes;
+        }
+        public ResultType Type { get; set; }
+        public int Reads { get; set; }
+        public int Writes { get; set; }
+
+        public Record Record { get; set; }
+
+        public override string ToString()
+        {
+            return $"Result: {Type}, Reads: {Reads}, Writes: {Writes}";
+        }
+    }
+}
